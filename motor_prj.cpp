2e78572b2591c41cd583c35a973da467470ab8a1
pieterlambers <75913721+pieterlambers@users.noreply.github.com>
2025-05-24 15:24:25 +0200

    motor saturation tests

--- motor_prj.cpp
+++ motor_prj.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "unit_test.h"
 #include "motor_simulation.h"
+#include "motor_simulation_test.h"
 
 int add(int a, int b) {
     return a + b;
@@ -14,6 +15,8 @@ int main() {
 
     UT_CheckInRange(5, 1, 10, "Five is in range 1-10");
     UT_CheckTrue("Addition check", add(2, 3) == 5);
+
+    Run_MotorTests();
 
     std::cout << "Press Enter to exit..." << std::endl;
     std::cin.get();
